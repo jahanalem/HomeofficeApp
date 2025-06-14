1a228c60d8388589d580c5019b027e4916aac68c
jahanalem <s.r.alem19@gmail.com>
2025-06-14 13:57:55 +0200

    feat: implement TokenService

--- Backend/Homeoffice/Homeoffice.Services/TokenService.cs
+++ Backend/Homeoffice/Homeoffice.Services/TokenService.cs
@@ -1,13 +1,46 @@
 ﻿using Homeoffice.Contracts.Services;
 using Homeoffice.Models.Entities.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Homeoffice.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        }
         public string CreateToken(User user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName!)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds,
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
